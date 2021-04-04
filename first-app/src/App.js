9efe4de9f35123e4e6d8b3eea81c4883aa95163d
Mark Misra <markmisra@MacBook-Air-Mark.local>
2021-04-04 19:01:45 +0300

    Add other components & React router dom

--- first-app/src/App.js
+++ first-app/src/App.js
@@ -1,20 +1,32 @@
 import './App.css';
+import { Route } from 'react-router';
+import { BrowserRouter } from 'react-router-dom';
+
+import Navigation from './components/navigation/Navigation';
+
 import Header from './components/header/Header';
 import Main from './components/main/Main';
-import Navigation from './components/navigation/Navigation';
 import Dialogs from './components/dialogs/Dialogs';
+import Settings from './components/settings/Settings';
+import Music from './components/music/Music';
+import News from './components/news/News';
 
 
 const App = () => {
   return (
-    <div className="app-wrapper">
-      <Header />
-      <Navigation />
-      <div className="app-wrapper-content">
-        {/* <Main /> */}
-        <Dialogs />
+    <BrowserRouter>
+      <div className="app-wrapper">
+        <Header />
+        <Navigation />
+        <div className="app-wrapper-content">
+          <Route path='/main' component={Main} />
+          <Route path='/dialogs' component={Dialogs} />
+          <Route path='/news' component={News} />
+          <Route path='/music' component={Music} />
+          <Route path='/settings' component={Settings} />
+        </div>
       </div>
-    </div>
+    </BrowserRouter>
   );
 }
 
--- first-app/src/components/navigation/Navigation.jsx
+++ first-app/src/components/navigation/Navigation.jsx
@@ -1,3 +1,4 @@
+import { NavLink } from 'react-router-dom';
 import classes from './Navigation.module.css'
 
 
@@ -5,11 +6,11 @@ import classes from './Navigation.module.css'
 const Navigation = () => {
   return (
     <nav className={classes.nav}>
-      <div className={classes.item}><a href="#">Profile</a></div>
-      <div className={classes.item}><a href="#">Messages</a></div>
-      <div className={classes.item}><a href="#">News</a></div>
-      <div className={classes.item}><a href="#">Music</a></div>
-      <div className={classes.item}><a href="#">Settings</a></div>
+      <div className={classes.item}><NavLink to="/main">Profile</NavLink></div>
+      <div className={classes.item}><NavLink to="/dialogs">Messages</NavLink></div>
+      <div className={classes.item}><NavLink to="/news">News</NavLink></div>
+      <div className={classes.item}><NavLink to="/music">Music</NavLink></div>
+      <div className={classes.item}><NavLink to="/settings">Settings</NavLink></div>
     </nav>
   );
 }
