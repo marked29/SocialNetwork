5a84491f6afc0f38adc2f692b3a34b8f52b698a2
Mark Misra <markmisra@MacBook-Air-Mark.local>
2021-05-22 16:14:46 +0300

    Add class component

--- first-app/src/components/users/Users.jsx
+++ first-app/src/components/users/Users.jsx
@@ -1,38 +1,57 @@
 import axios from 'axios';
+import * as React from 'react'
 import classes from './Users.module.css'
 const photourl = 'https://image.flaticon.com/icons/png/512/147/147144.png'
 
-const Users = (props) => {
-  debugger;
-  if (props.userPage.length === 0) {
-    axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
-      props.setUsers(response.data.items);
-    });
+class Users extends React.Component {
+  constructor(props) {
+    super(props)
   }
-  return (
-    <div >
-      {
-        props.userPage.map(u => <div>
-          <span>
-            <div><img src={u.photos.large} alt="avtr" className={classes.avatar_wrapper} /></div>
-            <div>{u.followed
-              ? <button onClick={() => { props.unfollow(u.id) }} >UNFOLLOW</button>
-              : <button onClick={() => { props.follow(u.id) }}>FOLLOW</button>}</div>
-          </span>
-          <span>
+
+  getUsers = () => {
+    if (this.props.userPage.length === 0) {
+      axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
+        this.props.setUsers(response.data.items);
+      });
+    }
+  }
+
+  componentDidMount() {
+    // Method to display that some events in lifecycle are happening 
+    this.getUsers()
+  }
+
+  componentDidUpdate() {
+    // Method to display that some events in lifecycle are happening 
+    alert("We have updated")
+  }
+
+  render() {
+    return(
+      <div>
+        {
+          this.props.userPage.map(u => <div>
             <span>
-              <div>{u.name}</div>
-              <div>{u.status}</div>
+              <div><img src={u.photos.large} alt="avtr" className={classes.avatar_wrapper} /></div>
+              <div>{u.followed
+                ? <button onClick={() => { this.props.unfollow(u.id) }} >UNFOLLOW</button>
+                : <button onClick={() => { this.props.follow(u.id) }}>FOLLOW</button>}</div>
             </span>
             <span>
-              {/* <div>{u.location.country}</div>
-              <div>{u.location.city}</div> */}
+              <span>
+                <div>{u.name}</div>
+                <div>{u.status}</div>
+              </span>
+              <span>
+                {/* <div>{u.location.country}</div>
+                <div>{u.location.city}</div> */}
+              </span>
             </span>
-          </span>
-        </div>)
-      }
-    </div>
-  );
+          </div>)
+        }
+      </div>
+    );
+  }
 }
 
 export default Users;
